288d27ed3d1ad3ed3e32b60e68b61cc3d618ec83
Blxee <aminedev57@gmail.com>
2026-02-07 13:31:09 +0100

    Changed: the coder thread is now a stand-alone function

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,31 @@ use std::sync::Arc;
 use std::thread::spawn;
 use std::time::Instant;
 
-use crate::args::parse_args;
+use crate::args::{Args, parse_args};
 use crate::coder::Coder;
 use crate::dongle::Dongle;
+
+fn coder_thread(
+    coder_nb: u64,
+    program_args: Args,
+    dongles: [Arc<Dongle>; 2],
+    program_start: Instant,
+) {
+    let mut coder = Coder::new(
+        coder_nb,
+        program_args.number_of_compiles_required,
+        dongles,
+        program_args.time_to_compile,
+        program_args.time_to_debug,
+        program_args.time_to_refactor,
+    );
+
+    while coder.compiles_left > 0 {
+        coder.compile(program_start);
+        coder.debug(program_start);
+        coder.refactor(program_start);
+    }
+}
 
 fn main() {
     let program_args = parse_args();
@@ -37,20 +59,7 @@ fn main() {
         let dongles = [Arc::clone(left_dongle), Arc::clone(right_dongle)];
 
         thread_handles.push(spawn(move || {
-            let mut coder = Coder::new(
-                i + 1,
-                program_args.number_of_compiles_required,
-                dongles,
-                program_args.time_to_compile,
-                program_args.time_to_debug,
-                program_args.time_to_refactor,
-            );
-
-            while coder.compiles_left > 0 {
-                coder.compile(program_start);
-                coder.debug(program_start);
-                coder.refactor(program_start);
-            }
+            coder_thread(i + 1, program_args, dongles, program_start)
         }));
     }
 
