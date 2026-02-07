663a37d3a36cfbfd26decf16b0e86669e05bf586
Blxee <aminedev57@gmail.com>
2026-02-07 12:10:25 +0100

    Added: working one dongle experimentation

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ mod args;
 mod coder;
 mod dongle;
 
-use std::sync::Arc;
-use std::thread::spawn;
-use std::time::Instant;
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread::{sleep, spawn};
+use std::time::{Duration, Instant, UNIX_EPOCH};
 
 use crate::args::parse_args;
 use crate::coder::Coder;
 use crate::dongle::Dongle;
 
-fn main() {
+fn foo() {
     let program_args = parse_args();
 
     let mut thread_handles = vec![];
@@ -58,4 +58,59 @@ fn main() {
     thread_handles.into_iter().for_each(|t| {
         let _ = t.join();
     });
+}
+
+fn coder(dongle: Arc<(Mutex<Instant>, Condvar)>, nb: i32) {
+    let (mx, cv) = &*dongle;
+    let mut guard = mx.lock().unwrap();
+    let mut next_avail = *guard;
+
+    while Instant::now() < next_avail {
+        (guard, _) = cv.wait_timeout(guard, next_avail - Instant::now()).unwrap();
+        next_avail = *guard;
+    }
+
+    *guard = Instant::now() + Duration::from_millis(3000);
+    drop(guard);
+
+    println!("coder {nb} is coding..");
+    sleep(Duration::from_secs(1));
+    println!("coder {nb} finished coding..");
+    cv.notify_all();
+}
+
+fn main() {
+    println!("started..");
+    let dongle = Arc::new((
+        Mutex::new(Instant::now() - Duration::from_secs(3)),
+        Condvar::new(),
+    ));
+
+    let coder_dongle = Arc::clone(&dongle);
+    let coder1 = spawn(|| coder(coder_dongle, 1));
+
+    let coder_dongle = Arc::clone(&dongle);
+    let coder2 = spawn(|| coder(coder_dongle, 2));
+
+    let (mx, cv) = &*dongle;
+    let mut guard = mx.lock().unwrap();
+
+    spawn(|| {
+        let start = Instant::now();
+        for _ in 0..20 {
+            println!("time: {:10?}", start.elapsed().as_secs());
+            sleep(Duration::from_millis(500));
+        }
+    });
+
+    for _ in 0..2 {
+        guard = cv.wait(guard).unwrap();
+        if guard.elapsed() < Duration::from_millis(2000) {
+            println!("dongle cooling down..");
+        }
+    }
+
+    coder1.join().unwrap();
+    coder2.join().unwrap();
+    println!("finished..");
 }
