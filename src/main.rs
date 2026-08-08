d37b1fbf989fb359163ce51b4bcaa407ab91c603
Blxee <aminedev57@gmail.com>
2026-08-08 23:11:54 +0100

    Restarting codexion

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-mod args;
-mod coder;
-mod dongle;
-mod quantum_table;
-
-use crate::args::parse_args;
-use crate::quantum_table::QuantumTable;
-
 fn main() {
-    let program_args = parse_args();
-
-    let quantum_table = QuantumTable::new(program_args);
-
-    quantum_table.start_coding();
+    println!("hello world");
 }
